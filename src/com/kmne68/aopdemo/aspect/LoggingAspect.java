e51e9565e0bb26008d415ed2c847d30788d9c981
Keith Emery <kmne68@gmail.com>
2020-08-24 00:07:41 -0500

    Clean code for LoggingAspect

--- src/com/kmne68/aopdemo/aspect/LoggingAspect.java
+++ src/com/kmne68/aopdemo/aspect/LoggingAspect.java
@@ -12,28 +12,6 @@ import org.springframework.stereotype.Component;
 public class LoggingAspect {
 	
 	
-	// add all our related advices
-	
-	
-	// start with an @Before advice
-	// @Before("execution(public void updateAccount())")
-	// @Before("execution(public void addAccount())")
-	// @Before("execution(public void com.kmne68.aopdemo.dao.AccountDAO.addAccount())")
-	// @Before("execution(public void add*())")
-	// @Before("execution(void add*())")
-	
-	// the following has a return type of "*" and matches on any method that begins with the string "add", 
-	// will match on any number of arguments (..)
-	// @Before("execution(* add*(com.kmne68.aopdemo.Account, ..))")
-	// @Before("execution(* add*(..))")
-	
-	// return type = *
-	// package = com.kmne68.aopdemo.dao
-	// class = *
-	// method = *
-	// params = ..
-	
-	
 	@Before("com.kmne68.aopdemo.aspect.AopExpressions.forDaoPackageNoGetterSetter()")
 	public void beforeAddAccountAdvice() {
 		
